e9e6d3c686043faf0eb04c3ce2ebe152c78a6685
Maroun Baydoun <maroun.baydoun@gmail.com>
2021-02-06 00:21:25 +0100

    Cleaned generated files before build

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,40 @@
-const path = require('path');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const PATHS = {
-  src: path.join(__dirname, './src'),
-  dist: path.join(__dirname, './dist')
-}
+  src: path.join(__dirname, "./src"),
+  dist: path.join(__dirname, "./dist"),
+};
 
 module.exports = {
-
   entry: {
-    'react-appear-in': path.join(PATHS.src, 'react-appear-in.tsx'),
+    "react-appear-in": path.join(PATHS.src, "react-appear-in.tsx"),
   },
   output: {
     path: PATHS.dist,
-    filename: '[name].js',
-    library: '[name]',
-    libraryTarget: 'umd',
-    umdNamedDefine: true
+    filename: "[name].js",
+    library: "[name]",
+    libraryTarget: "umd",
+    umdNamedDefine: true,
   },
   module: {
     rules: [
       {
         test: /(\.ts|\.tsx)/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
-      }
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: [".ts", ".tsx", ".js"],
   },
-  externals : {
-    react: 'react'
+  externals: {
+    react: "react",
   },
-}
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [path.join(process.cwd(), "types")],
+    }),
+  ],
+};
